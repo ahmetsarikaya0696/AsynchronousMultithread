0b706653a436e36a751669aa56302fc6c339cb6a
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-10-28 13:36:15 +0300

    ContinueWith() callback function

---
+++ TaskConsoleApp/Callback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskConsoleApp
+{
+    public static class Callback
+    {
+        public static void Work(Task<string> data)
+        {
+            // 100 satırlık kod
+            Console.WriteLine(data.Result.Length);
+        }
+    }
+}
---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using TaskConsoleApp;
+
+//var myTask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith((data) =>
+//{
+//    Console.WriteLine(data.Result.Length);
+//});
+
+var myTask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith((data) =>
+{
+    Callback.Work(data);
+});
+
+Console.WriteLine("Arada yapılacak işler");
+
+await myTask;
