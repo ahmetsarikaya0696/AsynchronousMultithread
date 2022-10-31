13281f649012dc7ae0554c6406a5bdc1cb2b12f1
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-10-31 20:29:27 +0200

    ParallelForEach ile Multithread programlama

---
+++ ForEachParallelApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace ForEachParallelApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Path : ");
+            string picturesPath = Console.ReadLine();
+            var files = Directory.GetFiles(picturesPath);
+         
+            Parallel.ForEach(files, (file) =>
+            {
+                Console.WriteLine($"Thread Id : {Thread.CurrentThread.ManagedThreadId}");
+                Image img = new Bitmap(file);
+                var thumbnail = img.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+                thumbnail.Save(Path.Combine(picturesPath, "thumbnails", Path.GetFileName(file)));
+            });
+
+            Console.WriteLine("İşlem bitti");
+        }
+    }
+}
