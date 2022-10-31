140d14151912fee2d7c70345e0d760c8843fbe95
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-10-31 13:44:20 +0200

    TaskFlow

---
+++ TaskFlow/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TaskFlow
+{
+    internal class Program
+    {
+        // 1 4 2 5 3
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("1. Adım");
+            var myTask = GetContent();
+
+            Console.WriteLine("2. Adım");
+            var content = await myTask;
+
+            Console.WriteLine("3. Adım");
+
+        }
+
+        public static async Task<string> GetContent()
+        {
+            Console.WriteLine("4. Adım");
+            var content = await new HttpClient().GetStringAsync("https://www.google.com");
+
+            Console.WriteLine("5. Adım");
+            return content;
+        }
+    }
+}
