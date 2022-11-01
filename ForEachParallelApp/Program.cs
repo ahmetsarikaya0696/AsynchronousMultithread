0c074b0a56dc91c9fbd34d4b588eb65625bb9b89
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-11-01 10:47:12 +0200

    ParallelForEach-2 Interlocked.Add()

--- ForEachParallelApp/Program.cs
+++ ForEachParallelApp/Program.cs
@@ -6,10 +6,35 @@ namespace ForEachParallelApp
     {
         static void Main(string[] args)
         {
+            ParallelForEach1();
+            ParallelForEach2();
+        }
+
+        private static void ParallelForEach2()
+        {
+            long filesByte = 0;
+
             Console.Write("Path : ");
             string picturesPath = Console.ReadLine();
             var files = Directory.GetFiles(picturesPath);
-         
+
+            Parallel.ForEach(files, (file) =>
+            {
+                Console.WriteLine($"Thread Id : {Thread.CurrentThread.ManagedThreadId}");
+                FileInfo fileInfo = new FileInfo(file);
+
+                Interlocked.Add(ref filesByte, fileInfo.Length); // Race Condition durumunu önler
+            });
+
+            Console.WriteLine("İşlem bitti");
+        }
+
+        private static void ParallelForEach1()
+        {
+            Console.Write("Path : ");
+            string picturesPath = Console.ReadLine();
+            var files = Directory.GetFiles(picturesPath);
+
             Parallel.ForEach(files, (file) =>
             {
                 Console.WriteLine($"Thread Id : {Thread.CurrentThread.ManagedThreadId}");
