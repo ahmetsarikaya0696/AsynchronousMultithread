996c8d09533b198c3aa914b269d96bf06d5c3467
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-11-01 12:12:34 +0200

    ParallelOptions, CancellationToken

---
+++ ParallelForForEachCancellationTokenApp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ParallelForForEachCancellationTokenApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCounter = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(341, 251);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCounter
+            // 
+            this.btnCounter.Location = new System.Drawing.Point(278, 12);
+            this.btnCounter.Name = "btnCounter";
+            this.btnCounter.Size = new System.Drawing.Size(75, 23);
+            this.btnCounter.TabIndex = 1;
+            this.btnCounter.Text = "Counter";
+            this.btnCounter.UseVisualStyleBackColor = true;
+            this.btnCounter.Click += new System.EventHandler(this.btnCounter_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(12, 298);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(341, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 329);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCounter);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCounter;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ ParallelForForEachCancellationTokenApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParallelForForEachCancellationTokenApp
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+        private int _counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            listBox1.Invoke((MethodInvoker)delegate { listBox1.Items.Clear(); });
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            List<string> urls = new List<string>()
+            {
+                "https://www.trendyol.com",
+                "https://www.twitch.tv/",
+                "https://twitter.com/",
+                "https://www.trendyol.com",
+                "https://www.twitch.tv/",
+                "https://twitter.com/",
+                "https://www.trendyol.com",
+                "https://www.twitch.tv/",
+                "https://twitter.com/",
+                "https://www.trendyol.com",
+                "https://www.twitch.tv/",
+                "https://twitter.com/",
+                "https://www.trendyol.com",
+                "https://www.twitch.tv/",
+                "https://twitter.com/",
+                "https://www.trendyol.com",
+                "https://www.twitch.tv/",
+                "https://twitter.com/"
+            };
+
+            HttpClient httpClient = new HttpClient();
+
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.CancellationToken = _cancellationTokenSource.Token;
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    Parallel.ForEach(urls, parallelOptions, (url) =>
+                    {
+                        string content = httpClient.GetStringAsync(url).Result;
+                        string data = $"Url : {url} Length : {content.Length}";
+
+                        _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                        listBox1.Invoke((MethodInvoker)delegate { listBox1.Items.Add(data); });
+                    });
+                }
+                catch (OperationCanceledException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Genel bir hata meydana geldi.");
+                }
+            });
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
+        }
+
+        private void btnCounter_Click(object sender, EventArgs e)
+        {
+            btnCounter.Text = _counter++.ToString();
+        }
+    }
+}
