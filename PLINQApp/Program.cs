64c6ba65bcef261ce05981ab722fe68b57a017d9
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-11-02 12:40:57 +0200

    AsParallel

---
+++ PLINQApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PLINQApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Enumerable.Range(1, 100).ToList();
+            var evens = array.AsParallel().Where(GetEvens).ToList();
+            
+            evens.ForEach(even =>
+            {
+                Console.WriteLine(even);
+            }) ;
+        }
+     
+        private static bool GetEvens(int x)
+        {
+            return x % 2 == 0;
+        }
+    }
+}
