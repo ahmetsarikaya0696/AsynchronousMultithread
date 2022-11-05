299537ee6afea37ee2433241663131721394246d
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-11-05 16:21:53 +0300

    PLINQCancellation

---
+++ PLINQCancellationTokenApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PLINQCancellationTokenApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(32, 28);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "PLINQ Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(113, 28);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(32, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(156, 329);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ PLINQCancellationTokenApp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PLINQCancellationTokenApp
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+        public Form1()
+        {
+            InitializeComponent();
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        private bool Mod12(int number)
+        {
+            Thread.SpinWait(50000); // Gerçek bir hesaplama işlemi yapılmış gibi 500 iterasyon geciktirir.
+            return number % 12 == 0;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        Enumerable.Range(1, 100000).AsParallel().WithCancellation(_cancellationTokenSource.Token).Where(Mod12).ToList().ForEach(x =>
+                            {
+                                Thread.Sleep(100);
+                                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                                listBox1.Invoke((MethodInvoker)delegate { listBox1.Items.Add(x); });
+                            });
+                    }
+                    catch (OperationCanceledException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                });
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
+        }
+    }
+}
